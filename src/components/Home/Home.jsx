d0bb0ed7805cd7d96507e10fb167bf6d075135e3
Rajguru-Aditya <rajguru.aditya.25@gmail.com>
2022-03-10 17:27:49 +0530

    Create banner image for home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,18 @@ import "./homeStyles.css";
 function Home() {
   return (
     <div className="home">
-      <h1>Home</h1>
+      <div className="banner">
+        <img
+          src="https://i.ibb.co/DbzFf5y/Orange-Rectangle.png"
+          alt="Orange-Rectangle"
+          className="banner-bg"
+        />
+        <img
+          src="https://i.ibb.co/sHHJ1h1/Login-BGHigher-Res.png"
+          alt="Login-BGHigher-Res"
+          className="banner-img"
+        ></img>
+      </div>
     </div>
   );
 }
