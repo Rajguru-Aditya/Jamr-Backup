71808c9990cbd110a422cc750764fde3cb8d69e8
Rajguru-Aditya <rajguru.aditya.25@gmail.com>
2022-03-12 22:00:03 +0530

    Create top image for Studios listing page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import Home from "./components/Home/Home";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Searchbar from "./components/Searchbar/Searchbar";
+import StudioListing from "./components/StudioListing/StudioListing";
+import FilterBar from "./components/FilterBar/FilterBar";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
       <Searchbar />
-      <Home />
+      {/* <Home /> */}
+      <StudioListing />
+      <FilterBar />
     </div>
   );
 }
---
+++ src/components/StudioListing/StudioListing.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./styles.css";
+
+function StudioListing() {
+  return (
+    <div className="studioListing">
+      <div className="studioListing-image">
+        <img
+          src="https://i.ibb.co/RycX0TC/wavy-Orange.png"
+          alt="wavy-Orange"
+          className="wavy-orange"
+        ></img>
+        <img
+          src="https://i.ibb.co/p2ZTCq0/listing-Page-Vector.png"
+          alt="listing-Page-Vector"
+          className="listing-Page-Vector"
+        ></img>
+        <h1 className="header">Music Studios</h1>
+      </div>
+    </div>
+  );
+}
+
+export default StudioListing;
