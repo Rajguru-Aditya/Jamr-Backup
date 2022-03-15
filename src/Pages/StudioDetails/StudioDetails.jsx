bd364011a132c07236f0d42a5b9434e78ffb5b9c
Rajguru-Aditya <rajguru.aditya.25@gmail.com>
2022-03-16 00:09:56 +0530

    Add reviews and date time components

--- src/Pages/StudioDetails/StudioDetails.jsx
+++ src/Pages/StudioDetails/StudioDetails.jsx
@@ -23,6 +23,37 @@ function StudioDetails() {
             alt="studio"
             className="studio-image"
           />
+
+          <div className="date-time-slots-container">
+            <div className="date-time-container">
+              <p className="date">Tuesday 15 March, 2022</p>
+              <p className="time">4pm to 7pm (3hrs)</p>
+            </div>
+            <div className="date-container">
+              <p className="date-title">Date</p>
+              <div className="date-inner-container">
+                <p>Tuesday 15 March, 2022</p>
+              </div>
+            </div>
+            <div className="time-duration-container">
+              <div className="time-container">
+                <p className="time-title">Start Time</p>
+                <div className="time-inner-container">
+                  <p>16:00</p>
+                </div>
+              </div>
+              <div className="duration-container">
+                <p className="duration-title">Duration</p>
+                <div className="duration-inner-container">
+                  <p>3 hrs</p>
+                </div>
+              </div>
+            </div>
+            <div className="slots-container"></div>
+            <div className="book-now-btn">
+              <p>Book Now</p>
+            </div>
+          </div>
         </div>
         <div className="studioDetails-right-container">
           <div className="studioDetails-info">
@@ -88,6 +119,46 @@ function StudioDetails() {
                   Headphones-2 x Pioneer HRM-5 Headphones
                 </p>
               </div>
+            </div>
+            <div className="service-ratings-reviews-container">
+              <div className="service-overall-ratings">
+                <div className="ratings-container">
+                  <p className="service-ratings-number">4.0/5</p>
+                  <p className="service-ratings-text">Overall Ratings</p>
+                </div>
+                <div className="verified-ratings">
+                  <p>⭐⭐⭐⭐</p>
+                  <p className="verified-ratings-text">
+                    1000+ verified ratings
+                  </p>
+                </div>
+              </div>
+              <div className="service-review-btn-container">
+                <div utton className="service-review-btn">
+                  Write a Review
+                </div>
+              </div>
+            </div>
+            <p className="reviews-title">Reviews</p>
+            <div className="reviews-container">
+              <div className="review-user-container">
+                <div className="review-user-img" />
+                <p className="review-username">Aditya Rajguru</p>
+              </div>
+              <p className="review-text">
+                "I had a wonderful experience working with the Sound Engineer
+                and Associates."
+              </p>
+            </div>
+            <div className="reviews-container">
+              <div className="review-user-container">
+                <div className="review-user-img" />
+                <p className="review-username">Aditya Rajguru</p>
+              </div>
+              <p className="review-text">
+                "I had a wonderful experience working with the Sound Engineer
+                and Associates."
+              </p>
             </div>
           </div>
         </div>
