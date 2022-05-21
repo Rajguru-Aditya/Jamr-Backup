29880a77925e4bbf22e8c718a0cf8e749d135f52
Rajguru-Aditya <rajguru.aditya.25@gmail.com>
2022-05-21 18:23:10 +0530

    add the test code for razor pay

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -99,6 +99,73 @@ function Payment() {
 
   console.log("storeDetails", storeDetails);
 
+  //PAYMENT
+
+  const loadScript = (src) => {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+
+      script.onload = () => {
+        resolve(true);
+      };
+
+      script.onerror = () => {
+        resolve(false);
+      };
+
+      document.body.appendChild(script);
+    });
+  };
+
+  const ProceedPayment = async () => {
+    const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
+
+    if (!res) {
+      alert("Something went wrong");
+      return;
+    }
+
+    const options = {
+      key: "YOUR_KEY_ID", // Enter the Key ID generated from the Dashboard
+      amount: "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency: "INR",
+      name: "Acme Corp",
+      description: "Test Transaction",
+      image: "https://example.com/your_logo",
+      order_id: "order_9A33XWu170gUtm", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      handler: function (response) {
+        alert(response.razorpay_payment_id);
+        alert(response.razorpay_order_id);
+        alert(response.razorpay_signature);
+      },
+      prefill: {
+        name: "Gaurav Kumar",
+        email: "gaurav.kumar@example.com",
+        contact: "9999999999",
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+    const rzp1 = new window.Razorpay(options);
+    rzp1.on("payment.failed", function (response) {
+      alert(response.error.code);
+      alert(response.error.description);
+      alert(response.error.source);
+      alert(response.error.step);
+      alert(response.error.reason);
+      alert(response.error.metadata.order_id);
+      alert(response.error.metadata.payment_id);
+    });
+    rzp1.open();
+  };
+
   const PromoCode = () => (
     <div className="promotional-code">
       <p className="subtitle-promo-code">Apply a promotional code</p>
@@ -140,8 +207,9 @@ function Payment() {
       </div>
       <div
         className="proceed-payment-btn"
+        id="payment-button"
         onClick={() => {
-          transaction();
+          ProceedPayment();
         }}
       >
         <p>Proceed to pay</p>
