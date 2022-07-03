035d86a59763d1cf90e5c95909fbd3afb8c64d9b
Rajguru-Aditya <rajguru.aditya.25@gmail.com>
2022-07-03 11:50:59 +0530

    Fix profile image size

--- src/components/StudioUserProfile/StudioUserProfile.jsx
+++ src/components/StudioUserProfile/StudioUserProfile.jsx
@@ -70,7 +70,7 @@ function StudioUserProfile() {
             <div className="profile-upper-left-container">
               <div className="profile-img-container">
                 <img
-                  className="profile-image"
+                  className="user-profile-image"
                   src="https://images.unsplash.com/photo-1507114845806-0347f6150324?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
                   alt="profile"
                 />
